8b64340f8bd0514ee27eaaa740bc3e2a49b4cb4c
George Mathew <george.meg91@gmail.com>
2016-09-26 13:47:29 -0400

    Build File updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-URL="https://github.com/Devops-george2/JavaTest"
-mkdir -p build
-cd build
+#URL=$(pwd)
 branch=${1:-master}
-unset GIT_WORK_TREE
-git clone $URL
-echo $branch
-cd JavaTest
+#unset GIT_WORK_TREE
+#git clone $URL
+#echo $branch
+#cd JavaTest
 git checkout $branch
 mvn clean install -DskipTests=True
-cd ..
-rm -rf JavaTest
+#cd ..
+#rm -rf JavaTest
